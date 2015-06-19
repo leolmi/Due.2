796eb076f72eb590e00b5eb6080288b0ed5e1695
leolmi <leo.olmi@gmail.com>
2015-06-19 22:42:45 +0200

    fix

--- client/app/main/search.controller.js
+++ client/app/main/search.controller.js
@@ -4,9 +4,20 @@
 'use strict';
 
 angular.module('due2App')
+  .filter('options', [function() {
+    return function(items, state, expired) {
+      var filtered = [];
+      if (items && items.length>0) {
+        items.forEach(function (i) {
+          if ((!state || i.data.state != 2) && (!expired || i.data.expired))
+            filtered.push(i);
+        });
+      }
+      return filtered;
+    }
+  }])
   .controller('SearchCtrl', ['$scope','$http','Util','Logger', function($scope,$http,Util,Logger) {
     $scope.searchtext = '';
-    $scope.extendedFilter = {};
 
     $http.get('/api/dues/0/99999999999')
       .success(function(dues) {
